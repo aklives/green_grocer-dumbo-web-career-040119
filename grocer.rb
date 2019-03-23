672151446268cef61ec61bc1e9c49cf22d30cf9f
Adam Kearney <aklives@hotmail.com>
2019-03-23 14:13:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,23 +11,23 @@ new_cart
 end
 
 def apply_coupons(cart, coupons)
-  
+
     fruit_name = coupons[:item]
     new_coupon_hash = {
       :price => coupons[:cost],
       :clearance => "true",
       :count => coupons[:num]
     }
-    
+
      if cart.key?(fruit_name)
       new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
       if cart[fruit_name][:count]>= coupons[:num]
-  
+
         cart[fruit_name][:count] -= coupons[:num]
       end
-      cart[fruit_name + " W/COUPON"] = new_coupon_hash 
+      cart[fruit_name + " W/COUPON"] = new_coupon_hash
     end
-   
+
   return cart
 end
 

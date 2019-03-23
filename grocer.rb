5db2e44e13abb0102a87b0b8c09bde6224302936
Adam Kearney <aklives@hotmail.com>
2019-03-23 13:32:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -10,9 +10,34 @@ def consolidate_cart cart
 new_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
+def apply_coupon cart, coupons
+  if coupons.size < 3
+    coupons.each do |c_hash|
+      fruit = c_hash[:item]
+      new_coupon_hash = {
+        price: c_hash[:cost],
+        clearance: true,
+        count: c_hash[:num]
+      }
+    end
+  end
+  if coupons.size == 3
+    fruit = coupons[:item]
+    new_coupon_hash = {
+      price: coupons[:cost],
+      clearance: true,
+      count: coupons[:num]
+    }
+  
+    cart.each do |item, cart_hash|
+      if item == coupons[:item]
+        cart_hash[:count] -= coupons[:num]
+      end
+    end
+    cart["#{fruit} W/COUPON"] = new_coupon_hash
+  end
+cart   
+end   
 
 def apply_clearance(cart)
   # code here
